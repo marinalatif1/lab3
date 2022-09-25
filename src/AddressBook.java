0373d7b5b186b51963106ebebaa708f0df89849c
marinaiskander <marinalatif@cmail.carleton.ca>
2022-09-25 14:21:34 -0400

    I have successfully uploaded my project onto GitHub to collaborate with other developers.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,26 +1,37 @@
-import java.util.*;
+import java.util.ArrayList;
+
 public class AddressBook {
 
+    private ArrayList<BuddyInfo> myBuddies;
 
-    private ArrayList<BuddyInfo> myBuddies = new ArrayList<>();
+    public AddressBook(){
+        this.myBuddies = new ArrayList<>();
+    }
 
     public void addBuddy(BuddyInfo buddy) {
-        myBuddies.add(buddy);
+        if (buddy != null) {
+            this.myBuddies.add(buddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-       myBuddies.remove(buddy);
+    public void removeBuddy(int index) {
+        if (index >= 0 && index < this.myBuddies.size()) {
+            this.myBuddies.remove(index);
+        }
+
     }
+
     public static void main(String[] args) {
+
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
 
+        //I have successfully uploaded my project onto GitHub to collaborate with other developers.
 
     }
 
+
 }
-
-
 	
